d54115de3bc99ed9420eb8aa7614951d2946a4b5
alexsenokosov <alexsenokosov666@gmail.com>
2023-02-12 16:57:47 +0300

    DZ_2

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 
 website = "https://jsonplaceholder.typicode.com/posts/1"
 print(requests.get(website).json())
-response = requests.put(website,data={
+response = requests.delete(website,data={
     "id": 1,
     "userId":12,
     "title": "my new post",
-    "body": "body for my post"
+
 })
-print(response.json())
+print(response)
